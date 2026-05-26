f7375001d9a1ff35940413b239dde0c1818e7bcf
alanalisbinski <142310431+alanalisbinski@users.noreply.github.com>
2026-05-26 12:43:21 -0300

    atualização para mobile

--- scripts.js
+++ scripts.js
@@ -5,46 +5,47 @@ menuHamburguer.addEventListener('click', () => {
     navbar.classList.toggle('ativo');
 });
 
-document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', () => {
-        navbar.classList.remove('ativo');
-    });
-});
-
 function abrirImagem(){
-    let imagem = document.getElementById("imagemProjeto").src;
-    document.getElementById("imagemGrande").src = imagem;
-    document.getElementById("imagemModal").style.display = "flex";
+let imagem = document.getElementById("imagemProjeto").src;
+document.getElementById("imagemGrande").src = imagem;
+document.getElementById("imagemModal").style.display = "flex";
 }
 
 function fecharImagem(){
-    document.getElementById("imagemModal").style.display = "none";
+document.getElementById("imagemModal").style.display = "none";
 }
 
 document.getElementById('btnMais').addEventListener('click', function() {
     document.getElementById('caixaTexto').style.display = 'block';
 });
+
 document.getElementById('btnFechar').addEventListener('click', function() {
     document.getElementById('caixaTexto').style.display = 'none';
 });
 
+
 document.getElementById('btnMais2').addEventListener('click', function() {
     document.getElementById('caixaTexto2').style.display = 'block';
 });
+
 document.getElementById('btnFechar2').addEventListener('click', function() {
     document.getElementById('caixaTexto2').style.display = 'none';
 });
 
+
 document.getElementById('btnMais3').addEventListener('click', function() {
     document.getElementById('caixaTexto3').style.display = 'block';
 });
+
 document.getElementById('btnFechar3').addEventListener('click', function() {
     document.getElementById('caixaTexto3').style.display = 'none';
 });
 
+
 document.getElementById('btnMais4').addEventListener('click', function() {
     document.getElementById('caixaTexto4').style.display = 'block';
 });
+
 document.getElementById('btnFechar4').addEventListener('click', function() {
     document.getElementById('caixaTexto4').style.display = 'none';
 });
