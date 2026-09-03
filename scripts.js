ca2c1d9a34f69b677743c8a4a7ba2a1501576dcf
Alana Cristina Lisbinski <alanalisbinski9@gmail.com>
2026-09-03 11:20:40 -0300

    ajuste de função ao abrir imagem

--- scripts.js
+++ scripts.js
@@ -1,49 +1,32 @@
-function abrirImagem(){
-
-let imagem = document.getElementById("imagemProjeto").src;
-
-document.getElementById("imagemGrande").src = imagem;
-
-document.getElementById("imagemModal").style.display = "flex";
-
+  function abrirImagem(caminho) {
+  const modal = document.getElementById('imagemModal');
+  const imagemGrande = document.getElementById('imagemGrande');
+  
+  imagemGrande.src = caminho;
+  modal.classList.add('ativo');
 }
 
-function fecharImagem(){
-
-document.getElementById("imagemModal").style.display = "none";
-
+function fecharImagem() {
+  const modal = document.getElementById('imagemModal');
+  modal.classList.remove('ativo');
 }
+
+document.getElementById('imagemModal').addEventListener('click', function(e) {
+  if (e.target === this) {
+    fecharImagem();
+  }
+});
 
 document.getElementById('btnMais').addEventListener('click', function() {
-    document.getElementById('caixaTexto').style.display = 'block';
+  document.getElementById('caixaTexto').style.display = 'block';
 });
-
 document.getElementById('btnFechar').addEventListener('click', function() {
-    document.getElementById('caixaTexto').style.display = 'none';
+  document.getElementById('caixaTexto').style.display = 'none';
 });
-
 
 document.getElementById('btnMais2').addEventListener('click', function() {
-    document.getElementById('caixaTexto2').style.display = 'block';
+  document.getElementById('caixaTexto2').style.display = 'block';
 });
-
 document.getElementById('btnFechar2').addEventListener('click', function() {
-    document.getElementById('caixaTexto2').style.display = 'none';
-});
-
-
-document.getElementById('btnMais3').addEventListener('click', function() {
-    document.getElementById('caixaTexto3').style.display = 'block';
-});
-
-document.getElementById('btnFechar3').addEventListener('click', function() {
-    document.getElementById('caixaTexto3').style.display = 'none';
-});
-
-document.getElementById('btnMais4').addEventListener('click', function() {
-    document.getElementById('caixaTexto4').style.display = 'block';
-});
-
-document.getElementById('btnFechar4').addEventListener('click', function() {
-    document.getElementById('caixaTexto4').style.display = 'none';
+  document.getElementById('caixaTexto2').style.display = 'none';
 });
